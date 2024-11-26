1b92d500bdfaf2781dd8b95d9898d9f2b6c1768a
Rich Neswold <rich.neswold@gmail.com>
2024-11-25 23:49:20 -0600

    :bug: make test work with recent `ClientID` change

--- test/drmem_clientid_test.dart
+++ test/drmem_clientid_test.dart
@@ -3,7 +3,7 @@ import "package:flutter_test/flutter_test.dart";
 
 void main() {
   test("... Client ID serialization", () {
-    final id = ClientID.fromJson({'clientID': "0817263544536271"});
+    final id = ClientID.fromJson({'fingerprint': "0817263544536271"});
 
     expect(id.fingerprint, equals("08:17:26:35:44:53:62:71"));
     expect(ClientID.fromJson(id.toJson()).fingerprint,
